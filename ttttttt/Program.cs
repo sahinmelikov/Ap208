cc2c45a4e6ac095f0e5e023f5b4a6e9deb838d17
sahinmelikov <1rima621melikov@gmail.com>
2023-03-08 20:09:36 +0400

    c#

---
+++ ttttttt/Program.cs
@@ -0,0 +1,23 @@
+﻿/////Task3
+/*int sayi;
+
+sayi = 32;
+
+if (sayi % 2 == 0)
+{
+
+    while (sayi%2==0)
+   
+    {
+        sayi /= 2;
+    }
+   if(sayi == 1)
+    {
+        Console.WriteLine("2 nin quvvetidir");
+    }
+    else
+    {
+        Console.WriteLine("2 nin quvveti deyildir");
+    }
+
+}*/
